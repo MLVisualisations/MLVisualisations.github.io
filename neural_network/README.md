bfdc5e9957e0642a527ce8b8ce013446a883d614
Steven Elsworth <steven.elsworth@sky.com>
2020-02-29 08:17:15 +0000

    update neural network

--- neural_network/README.md
+++ neural_network/README.md
@@ -1,37 +1,44 @@
 # Neural Networks
 
-### A neuron takes multiple inputs and produces one output:
+### A single neuron takes in multiple inputs (a vector) and produces one output (a scalar):
 ![](./img/fig1.png)
-### Inside the neuron we take a weighted sum, add a bias, and apply an activation function:
+### Inside the neuron we take a linear combination of the inputs, add a bias, and apply a (nonlinear) activation function:
 ![](./img/fig2.png)
 ### The activation function behaves like a switch:
 ![](./img/fig3.png)
-### And so a neuron partitions the input space where the weights control the direction:
+### And so a single neuron simply partitions the vector space. The weights control the direction of the partition:
 ![](./img/fig4.png)
 ### And the bias applies a shift:
 ![](./img/fig5.png)
 ### To create more partitions, we need more neurons:
 ![](./img/fig6.png)
-### And the output is a vector equal to number of neurons:
+### But now the output is a vector equal to number of neurons:
 ![](./img/fig7.png)
 ### We can represent the new space as node on a hypercube:
 ![](./img/fig8.png)
-### We can add an additional layer to partition this new space. Alternatively, we could sum the output of multiple neurons:
+### An additional layer can be used to partition this new space.
 ![](./img/fig9.png)
-### This combines regions:
+### And the output of this second layer is again a hyperplane:
 ![](./img/fig10.png)
-### And we can keep adding neurons:
+### The isolated datapoint [0,1] represents a full region in the first layer:
 ![](./img/fig11.png)
-### Suppose we wanted to classify the green crosses, with 4 neurons we could have:
-![](./img/fig12.png)
-### And with 8 neurons:
-![](./img/fig13.png)
-### If the classification problem became more complex, we could need many neurons:
-![](./img/fig14.png)
 
-### In practise, we use continous functions which approximate the step function
+### Consider the following classification problem:
+![](./img/fig12.png)
+### We need three hyperplanes to separate the regions (3 neurons):
+![](./img/fig13.png)
+### The output is three dimensional:
+![](./img/fig14.png)
+### We can visualise these points on a three dimensional hypercube where we can color the nodes according to their class:
 ![](./img/fig15.png)
+### A single neuron in a second layer can isolate the blue from the red:
 ![](./img/fig16.png)
-### And now the regions become less distinct and so it makes more sense to have deeper networks
+### Which corresponds to the following partition:
 ![](./img/fig17.png)
+### As we have shown, when using a step function, the first layer divides the space into regions. Two points in the same region are indistinguishable. And the second layer merges regions.
+
+### As the classification problem gets more difficult, we need more neurons:
 ![](./img/fig18.png)
+### And this is only a 2 dimensional problem. In practise, we use continous functions instead of the step function:
+![](./img/fig19.png)
+### The region is no longer mapped to two distinct points, and so subsequent layers do a lot more. 
--- neural_network/neural_network.py
+++ neural_network/neural_network.py
@@ -2,6 +2,7 @@ import numpy as np
 np.random.seed(0)
 import matplotlib.pyplot as plt
 from scipy import stats
+from mpl_toolkits.mplot3d import Axes3D
 plt.style.use('fast')
 
 
@@ -27,7 +28,7 @@ def step(x):
 #############
 fig, axes = plt.subplots(ncols=1, nrows=1)
 axes.plot([-2, 0, 0, 2], [0, 0, 1, 1], 'k')
-plt.yticks(ticks = [0, 1], labels=['on', 'off'])
+plt.yticks(ticks = [0, 1], labels=['off', 'on'])
 plt.title('$\sigma(x)$')
 plt.savefig('img/fig3')
 
@@ -170,134 +171,100 @@ plt.savefig('img/fig11')
 #############
 # Figure 12
 #############
-# Sum neurons
+x_coord = [0.90, 0.71, 0.79, 0.50, 0.69, 0.61, 0.98, 0.76, 0.77, 0.22, 0.45, 0.57, 0.59, 0.80, 0.91, 0.99, 0.24, 0.4, 0.35]
+y_coord = [0.43, 0.52, 0.63, 0.80, 0.35, 0.72, 0.04, 0.19, 0.27, 0.61, 0.85, 0.25, 0.46, 0.81, 0.70, 0.99, 0.30, 0.95, 0.48]
+label = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+fig, axes = plt.subplots(ncols=1, nrows=1)
+for i, val in enumerate(label):
+    if val == 1:
+        axes.plot(x_coord[i], y_coord[i], 'ro')
+    else:
+        axes.plot(x_coord[i], y_coord[i], 'bo')
+axes.set(xlim=(0, 1), ylim=(0, 1), aspect=1)
+plt.savefig('img/fig12')
 
 #############
 # Figure 13
 #############
-fig, axes = plt.subplots(ncols=1, nrows=1)
-lx = np.linspace(-1, 1, 100)
-ly = np.linspace(-1, 1, 100)
-D1 = np.zeros([100, 100])
-for i, x in enumerate(lx):
-    for j, y in enumerate(ly):
-        if step(x):
-            D1[i, j] += 1
-        if step(x + y):
-            D1[i, j] += 1
-pos = axes.imshow(D1, extent = [-1, 1, -1, 1], cmap=plt.get_cmap('Greys'), aspect = 1)
-axes.title.set_text('$\sigma(x) + \sigma(x+y)$')
-fig.colorbar(pos, shrink=1)
+plt.plot([0, 1], [0.98, 0.65], 'k')
+plt.plot([0.64, 0.64], [0, 1], 'k')
+plt.plot([0.2, 1], [1, 0.2], 'k')
 plt.savefig('img/fig13')
 
 #############
 # Figure 14
 #############
-fig, axes = plt.subplots(ncols=1, nrows=1)
-lx = np.linspace(-1, 1, 100)
-ly = np.linspace(-1, 1, 100)
-D2 = np.zeros([100, 100])
-for i, x in enumerate(lx):
-    for j, y in enumerate(ly):
-        if step(-x + 2*y + 0.4):
-            D2[i, j] += 1
-        if step(x + y + 0.4):
-            D2[i, j] += 1
-        if step(x):
-            D2[i, j] += 1
-axes.imshow(D2, extent = [-1, 1, -1, 1], cmap=plt.get_cmap('Greys'), aspect = 1)
-axes.title.set_text('$\sigma(-x + 2y + 0.4) + \sigma(x + y + 0.4) + \sigma(x)$')
-fig.colorbar(pos, shrink=1)
-plt.savefig('img/fig14')
+plt.text(0.75, 0.15, '[0, 0, 1]')
+plt.text(0.45, 0.75, '[0, 1, 0]')
+plt.text(0.75, 0.55, '[0, 1, 1]')
 
+plt.text(0.75, 0.9, '[1, 1, 1]')
+plt.text(0.3, 0.4, '[0, 0, 0]')
+plt.text(0.4, 0.9, '[1, 1, 0]')
+plt.text(0.07, 0.96, '[1, 0, 0]')
+plt.savefig('img/fig14')
 
 #############
 # Figure 15
 #############
-fig, axes = plt.subplots(ncols=1, nrows=1)
-r = 0.5*np.random.rand(200)
-theta = 2*np.pi*np.random.rand(200)
-axes.plot(r*np.cos(theta), r*np.sin(theta), 'gx')
-lx = np.linspace(-1, 1, 100)
-ly = np.linspace(-1, 1, 100)
-D1 = np.zeros([100, 100])
-for i, x in enumerate(lx):
-    for j, y in enumerate(ly):
-        if step(x + 0.5):
-            D1[i, j] += 1
-        if step(-x + 0.5):
-            D1[i, j] += 1
-        if step(y + 0.5):
-            D1[i, j] += 1
-        if step(-y + 0.5):
-            D1[i, j] += 1
-pos = axes.imshow(D1, extent = [-1, 1, -1, 1], cmap=plt.get_cmap('Greys'), aspect = 1)
-fig.colorbar(pos, shrink=1)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter([0, 0, 0], [0, 1, 1], [1, 0, 1], c='b')
+ax.scatter([1, 0, 1, 1], [1, 0, 1, 0], [1, 0, 0, 0], c='r')
 plt.savefig('img/fig15')
 
 #############
 # Figure 16
 #############
-fig, axes = plt.subplots(ncols=1, nrows=1)
-r = 0.5*np.random.rand(200)
-theta = 2*np.pi*np.random.rand(200)
-axes.plot(r*np.cos(theta), r*np.sin(theta), 'gx')
-lx = np.linspace(-1, 1, 100)
-ly = np.linspace(-1, 1, 100)
-D1 = np.zeros([100, 100])
-for i, x in enumerate(lx):
-    for j, y in enumerate(ly):
-        if step(x + 0.5):
-            D1[i, j] += 1
-        if step(-x + 0.5):
-            D1[i, j] += 1
-        if step(y + 0.5):
-            D1[i, j] += 1
-        if step(-y + 0.5):
-            D1[i, j] += 1
-        if step(x + y + 0.71):
-            D1[i, j] += 1
-        if step(-x - y + 0.71):
-            D1[i, j] += 1
-        if step(-x + y + 0.71):
-            D1[i, j] += 1
-        if step(x - y + 0.71):
-            D1[i, j] += 1
-pos = axes.imshow(D1, extent = [-1, 1, -1, 1], cmap=plt.get_cmap('Greys'), aspect = 1)
-fig.colorbar(pos, shrink=1)
+x = np.linspace(0, 1, 4)
+y = np.linspace(0, 1, 4)
+X, Y = np.meshgrid(x, y)
+ax.set_xlim([0,1])
+ax.set_ylim([0,1])
+ax.set_zlim([0,1])
+ax.view_init(50, -89)
 plt.savefig('img/fig16')
 
 #############
 # Figure 17
 #############
+x_coord = [0.90, 0.71, 0.79, 0.50, 0.69, 0.61, 0.98, 0.76, 0.77, 0.22, 0.45, 0.57, 0.59, 0.80, 0.91, 0.99, 0.24, 0.4, 0.35]
+y_coord = [0.43, 0.52, 0.63, 0.80, 0.35, 0.72, 0.04, 0.19, 0.27, 0.61, 0.85, 0.25, 0.46, 0.81, 0.70, 0.99, 0.30, 0.95, 0.48]
+label = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 fig, axes = plt.subplots(ncols=1, nrows=1)
-lx = np.linspace(-0.5, 0.5, 11)
-ly = np.linspace(-0.5, 0.5, 11)
-colors = ['r', 'b']
-for x in lx:
-    for y in ly:
-        axes.plot(x, y, 'x', color=colors[np.random.randint(2)])
-
-for x in lx:
-    axes.plot([x-0.05, x-0.05], [-1, 1], 'k')
-axes.plot([0.55, 0.55], [-1, 1], 'k')
-for y in ly:
-    axes.plot([-1, 1], [y-0.05, y-0.05], 'k')
-axes.plot([-1, 1], [0.55, 0.55], 'k')
-axes.set(xlim=(-1, 1), ylim=(-1, 1))
+for i, val in enumerate(label):
+    if val == 1:
+        axes.plot(x_coord[i], y_coord[i], 'ro')
+    else:
+        axes.plot(x_coord[i], y_coord[i], 'bo')
+axes.set(xlim=(0, 1), ylim=(0, 1), aspect=1)
+plt.plot([0, 1], [0.98, 0.65], 'k')
+plt.plot([0.64, 0.64], [0, 1], 'k')
+plt.plot([0.2, 1], [1, 0.2], 'k')
+axes.fill_between([0.64, 1], [0.77, 0.65], [0, 0], color = 'blue', alpha=0.5)
+axes.fill_between([0.33, 0.64], [0.87, 0.77], [0.87, 0.56], color = 'blue', alpha=0.5)
 plt.savefig('img/fig17')
 
 #############
 # Figure 18
 #############
-fig, (ax1, ax2) = plt.subplots(ncols=1, nrows=2)
-x = np.linspace(-8, 8, 1000)
-ax1.plot(x, sigmoid(x), 'k', label='$\sigma(x) = \exp(x)/(1 + \exp(x))$')
-ax2.plot(x, np.tanh(x), 'k', label='$\sigma(x) = tanh(x)$')
-ax1.set(xlim=(-8, 8), ylim=(-0.1, 1.1))
-ax2.set(xlim=(-8, 8), ylim=(-1.1, 1.1))
-ax1.legend()
-ax2.legend()
+x_coord = [0.08, 0.10, 0.90, 0.40, 0.71, 0.79, 0.50, 0.69, 0.61, 0.98, 0.76, 0.77, 0.22, 0.45, 0.57, 0.59, 0.80, 0.91, 0.99, 0.24, 0.4, 0.35]
+y_coord = [0.12, 0.82, 0.43, 0.18, 0.52, 0.63, 0.80, 0.35, 0.72, 0.04, 0.19, 0.27, 0.61, 0.85, 0.25, 0.46, 0.81, 0.70, 0.99, 0.30, 0.95, 0.48]
+label = [0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1]
+fig, axes = plt.subplots(ncols=1, nrows=1)
+for i, val in enumerate(label):
+    if val == 1:
+        axes.plot(x_coord[i], y_coord[i], 'ro')
+    else:
+        axes.plot(x_coord[i], y_coord[i], 'bo')
+axes.set(xlim=(0, 1), ylim=(0, 1), aspect=1)
+plt.plot([0, 1], [0.75, 0.9], 'k')
+plt.plot([0, 1], [0.5, 0.8], 'k')
+plt.plot([0, 1], [0.18, 0.5], 'k')
+plt.plot([0, 1], [0.38, 0.28], 'k')
+plt.plot([0.45, 0.6], [0, 1], 'k')
+plt.plot([0.9, 0.4], [0, 1], 'k')
 plt.savefig('img/fig18')
 
 #############
@@ -311,54 +278,74 @@ def leaky_relu(x):
         return x
     else:
         return 0.01*x
-
-fig, (ax1, ax2) = plt.subplots(ncols=1, nrows=2)
+fig, (ax1, ax2, ax3, ax4) = plt.subplots(ncols=1, nrows=4)
 x = np.linspace(-8, 8, 1000)
-ax1.plot(x, [relu(i) for i in x], 'k', label='$\sigma(x) = \max(x, 0)$')
-ax2.plot(x, [leaky_relu(i) for i in x], 'k', label='$\sigma(x) = x$ if $x > 0$, and $0.01x$ otherwise')
-ax1.set(xlim=(-8, 8), ylim=(-0.1, 8))
-ax2.set(xlim=(-8, 8), ylim=(-1.1, 8))
+ax1.plot(x, sigmoid(x), 'k', label='$\sigma(x) = \exp(x)/(1 + \exp(x))$')
+ax2.plot(x, np.tanh(x), 'k', label='$\sigma(x) = tanh(x)$')
+ax3.plot(x, [relu(i) for i in x], 'k', label='$\sigma(x) = \max(x, 0)$')
+ax4.plot(x, [leaky_relu(i) for i in x], 'k', label='$\sigma(x) = x$ if $x > 0$, and $0.01x$ otherwise')
+ax1.set(xlim=(-8, 8), ylim=(-0.1, 1.1))
+ax2.set(xlim=(-8, 8), ylim=(-1.1, 1.1))
+ax3.set(xlim=(-8, 8), ylim=(-0.1, 1.1))
+ax4.set(xlim=(-8, 8), ylim=(-1.1, 1.1))
+ax1.set_xticks([])
+ax2.set_xticks([])
+ax3.set_xticks([])
 ax1.legend()
 ax2.legend()
+ax3.legend()
+ax4.legend()
 plt.savefig('img/fig19')
 
 #############
 # Figure 20
 #############
-fig, axes = plt.subplots(ncols=1, nrows=1)
-lx = np.linspace(-1, 1, 100)
-ly = np.linspace(-1, 1, 100)
-D1 = np.zeros([100, 100])
+fig, axes = plt.subplots(ncols=2, nrows=2)
+plt.subplots_adjust(hspace=0.4)
+lx = np.linspace(-4, 4, 400)
+ly = np.linspace(-4, 4, 400)
+D1 = np.zeros([400, 400])
+D2 = np.zeros([400, 400])
+D3 = np.zeros([400, 400])
+D4 = np.zeros([400, 400])
 for i, x in enumerate(lx):
     for j, y in enumerate(ly):
-        D1[i, j] += sigmoid(8*x)
-        D1[i, j] += sigmoid(8*x+8*y)
+        D1[i, j] = sigmoid(x+0.4)
+        D2[i, j] = sigmoid(x + y+ 0.4)
+        D3[i, j] = sigmoid(2*x - y+ 0.4)
+        D4[i, j] = sigmoid(-x + 2*y+ 0.4)
+pos = axes[0, 0].imshow(D1, extent = [-4, 4, -4, 4], cmap=plt.get_cmap('Greys'), aspect = 1)
+axes[0, 0].title.set_text('$\sigma(x + 0.4)$')
+axes[0, 1].imshow(D2, extent = [-4, 4, -4, 4], cmap=plt.get_cmap('Greys'), aspect = 1)
+axes[0, 1].title.set_text('$\sigma(x + y + 0.4)$')
+axes[1, 0].imshow(D3, extent = [-4, 4, -4, 4], cmap=plt.get_cmap('Greys'), aspect = 1)
+axes[1, 0].title.set_text('$\sigma(2x - y + 0.4)$')
+axes[1, 1].imshow(D4, extent = [-4, 4, -4, 4], cmap=plt.get_cmap('Greys'), aspect = 1)
+axes[1, 1].title.set_text('$\sigma(-x + 2y + 0.4)$')
+fig.colorbar(pos, ax=axes[0:2], shrink=1)
+plt.savefig('img/fig20')
 
-pos = axes.imshow(D1, extent = [-1, 1, -1, 1], cmap=plt.get_cmap('Greys'), aspect = 1)
-axes.title.set_text('$\sigma(8*x) + \sigma(8*x+8*y)$')
-fig.colorbar(pos, shrink=1)
+
+"""
+#############
+# Figure 20
+#############
+fig, ax = plt.subplots(ncols=1, nrows=1)
+for i, x in enumerate(np.linspace(0.1, 0.9, 9)):
+    for j, y in enumerate(np.linspace(0.1, 0.9, 9)):
+        label = (j)*9 + i
+        #ax.plot(x, y, 'xk')
+        ax.text(x, y, str(label))
 plt.savefig('img/fig20')
 
 #############
 # Figure 21
 #############
-fig, axes = plt.subplots(ncols=1, nrows=1)
-r = 0.5*np.random.rand(200)
-theta = 2*np.pi*np.random.rand(200)
-axes.plot(r*np.cos(theta), r*np.sin(theta), 'gx')
-lx = np.linspace(-1, 1, 100)
-ly = np.linspace(-1, 1, 100)
-D1 = np.zeros([100, 100])
-for i, x in enumerate(lx):
-    for j, y in enumerate(ly):
-        D1[i, j] += sigmoid(x + 0.5)
-        D1[i, j] += sigmoid(y + 0.5)
-        D1[i, j] += sigmoid(-x + 0.5)
-        D1[i, j] += sigmoid(-y + 0.5)
-        D1[i, j] += sigmoid(x + y + 0.71)
-        D1[i, j] += sigmoid(-x - y + 0.71)
-        D1[i, j] += sigmoid(-x + y + 0.71)
-        D1[i, j] += sigmoid(x - y + 0.71)
-pos = axes.imshow(D1, extent = [-1, 1, -1, 1], cmap=plt.get_cmap('Greys'), aspect = 1)
-fig.colorbar(pos, shrink=1)
-plt.savefig('img/fig21')
+fig, ax = plt.subplots(ncols=1, nrows=1)
+for i, x in enumerate(np.linspace(0.1, 0.9, 9)):
+    for j, y in enumerate(np.linspace(0.1, 0.9, 9)):
+        label = (j)*9 + i
+        #ax.plot(x, y, 'xk')
+        ax.text(x, y, str(label))
+plt.savefig('img/fig20')
+"""
